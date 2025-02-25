4f5095c01005f6ba22568b2b7b6e4707d93e8940
Immortalgleam <olesya.zaets@gmail.com>
2025-02-25 11:01:40 +0200

    updated elements_test.py
    added replace method for curr_add, perm_add

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,9 +1,19 @@
 import time
 
-from pages.base_page import BasePage
+from pages.elements_page import TextBoxPage
 
 
-def test(driver):
-    page = BasePage(driver, 'https://www.google.com/')
-    page.open()
-    time.sleep(3)
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
+            current_address = current_address.replace("\n", " ")
+            permanent_address = permanent_address.replace("\n", " ")
+            assert full_name == output_name, "the full name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_cur_addr, "the current address does not match"
+            assert permanent_address == output_per_addr, "the permanent address does not match"
