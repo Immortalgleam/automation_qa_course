71560c47e0422fb2ab9936f16172a295e8916c61
Immortalgleam <olesya.zaets@gmail.com>
2025-05-14 22:23:29 +0300

    added tests:test_web_table_add_person, test_web_table_search_person

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -7,6 +9,11 @@ Faker.seed()
 def generated_person():
     yield Person(
         full_name=faker_eng.first_name() + " " + faker_eng.last_name(),
+        firstname=faker_eng.first_name(),
+        lastname=faker_eng.last_name(),
+        age=random.randint(10, 80),
+        department=faker_eng.job(),
+        salary=random.randint(10_000, 100_000),
         email=faker_eng.email(),
         current_address=faker_eng.address(),
         permanent_address=faker_eng.address(),
